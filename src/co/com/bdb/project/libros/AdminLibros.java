54abdf00f22339861ef6636b311cecc6b4945891
edimart28 <edimart28@gmail.com>
2024-09-20 17:19:06 -0500

    Se añadio un libro

--- src/co/com/bdb/project/libros/AdminLibros.java
+++ src/co/com/bdb/project/libros/AdminLibros.java
@@ -3,8 +3,12 @@ package co.com.bdb.project.libros;
 public class AdminLibros {
     public static void main(String[] args) {
 
-        Libros libroUno = new Libros("Principito", "Juan", 200, 1995);
-        Libros libroDos = new Libros("Pinocho", "Pedro", 182, 1985);
+        Libros libroUno = new Libros("Cien años de soledad", "Gabriel García Marquez", 125,1967);
+        Libros libroDos = new Libros("Delirio", "Laura Restrepo", 89,2004);
+        Libros libroTres = new Libros("El olvido que seremos", "Héctor Adad Faciolince", 225,2006);
+
+
+
 
         System.out.println("-----------------------------------");
         System.out.println("Libro 1:");
@@ -13,6 +17,10 @@ public class AdminLibros {
 
         System.out.println("Libro 2:");
         libroDos.mostrarDatosLibro();
+        System.out.println("-----------------------------------");
+
+        System.out.println("Libro 3:");
+        libroTres.mostrarDatosLibro();
         System.out.println("-----------------------------------");
 
 
